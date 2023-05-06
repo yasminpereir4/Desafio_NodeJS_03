2a7eb2bddcf306ff96e66fa35504334b8b70e1eb
Yasmin <yasmin.mota311@gmail.com>
2023-05-06 16:32:21 -0300

    Algoritmo para apresentar nomes e notas de alunos, e apresentar a maior nota. Fiz uso de loops, if,e functions.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { type } from 'os';
 
 dotenv.config();
 
@@ -36,8 +37,72 @@ app.use(express.json());
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
-
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+	const readline = require ('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+
+interface Aluno {
+  nome: string;
+  nota: number;
+}
+
+const alunos: Aluno[] = [];
+
+const cadastrarAlunos = (quantidade: number, contador: number = 0): void => {
+  if (contador < quantidade) {
+    rl.question(`Digite o nome do aluno ${contador + 1}: `, (nome: string) => {
+      rl.question(`Digite a nota do aluno ${contador + 1}: `, (nota: string) => {
+        const notaNumber = Number(nota);
+        if (isNaN(notaNumber)) {
+          console.log('Nota inválida, tente novamente!');
+          cadastrarAlunos(quantidade, contador);
+        } else {
+          alunos.push({ nome, nota: notaNumber });
+          cadastrarAlunos(quantidade, contador + 1);
+        }
+      });
+    });
+  } else {
+    console.log('Alunos cadastrados:');
+    alunos.forEach((aluno) => {
+      console.log(`- ${aluno.nome}: ${aluno.nota}`);
+    });
+    if (alunos.length > 0) {
+      const alunoComMaiorNota: Aluno | null = obterMaiorNota(alunos);
+      if (alunoComMaiorNota !== null) {
+        console.log(`O aluno com maior nota é ${alunoComMaiorNota.nome}, com a nota ${alunoComMaiorNota.nota}`);
+      }
+    }
+    rl.close();
+  }
+};
+
+rl.question('Quantos alunos deseja inserir? ', (quantidade: string) => {
+  const quantidadeNumber = Number(quantidade);
+  if (isNaN(quantidadeNumber)) {
+    console.log('Quantidade inválida, tente novamente!');
+    rl.close();
+  } else {
+    console.log(`A quantidade digitada foi: ${quantidadeNumber}`);
+    cadastrarAlunos(quantidadeNumber);
+  }
 });
+
+const obterMaiorNota = (alunos: Aluno[]): Aluno | null => {
+  let maiorNota: number = 0;
+  let alunoComMaiorNota: Aluno | null = null;
+  for (const aluno of alunos) {
+    if (aluno.nota > maiorNota) {
+      maiorNota = aluno.nota;
+      alunoComMaiorNota = aluno;
+    }
+  }
+  return alunoComMaiorNota;
+};
+
+	});
